9233491472ce6e559d80d695b522988d8f257c5b
monco-45-maker <monco-45@hotmail.com>
2026-03-05 21:39:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -70,10 +70,9 @@ maxWidth:"1000px",
 margin:"40px auto"
 }}>
 
-<img src={project1.src} alt="Painting Project 1" style={{width:"100%", borderRadius:"10px"}} />
-<img src={project2.src} alt="Painting Project 2" style={{width:"100%", borderRadius:"10px"}} />
-<img src={project3.src} alt="Painting Project 3" style={{width:"100%", borderRadius:"10px"}} />
-<img src={project4.src} alt="Painting Project 4" style={{width:"100%", borderRadius:"10px"}} />
+<img src={project1.src} alt="Painting Project 1" 
+<img src={project2.src} alt="Painting Project 2" 
+<img src={project4.src} alt="Painting Project 4" 
   
 </div>
       
